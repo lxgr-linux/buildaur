54117843d47592a359aa8acfe71d669f1ac58787
lxgr <lxgr@protonmail.com>
2020-10-09 19:20:06 +0200

    full_list

---
+++ full_list.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# This script can be used to genrate an complete list of all packages which are awailable from the AUR
+# This is not included in the baseinstall and is just for testing purposes
+
+for i in {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
+do
+	buildaur.py -Qsqq $i
+done
