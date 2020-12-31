c18e98609180811428c92fdf5f721c133addf30c
lxgr <lxgr@protonmail.com>
2020-12-31 18:58:04 +0100

    got it to work

--- deptree.py
+++ deptree.py
@@ -5,7 +5,18 @@ from buildaur import *
 def branch(pkgs):
     alldeps=[]
     for pkg in pkgs:
-        alldeps.append(deps(pkg))
-    for pkg in alldeps:
-        alldeps+=branch(pkg)
+        alldeps+=deps(pkg, do_install=False, quiet=True)
+    if len(alldeps) != 0:
+        alldeps+=branch(alldeps)
     return alldeps
+
+def main(names):
+    rescount, cutted=info(resolve(names))
+    for i in range(rescount):
+        pkg=Informer(cutted, i)
+        for pkg in branch([pkg]):
+            print(pkg.name, end=" ")
+    print("")
+
+if __name__ == "__main__":
+    main(["python2-wnck"])
